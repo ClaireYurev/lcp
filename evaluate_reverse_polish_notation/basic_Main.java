f689634b182d2d2c64cb4af54363738badbb5aa1
Claire Yaroslav Yurev <61437734+ClaireYurev@users.noreply.github.com>
2022-05-17 13:10:22 -0700

    Create basic_Main.java

---
+++ evaluate_reverse_polish_notation/basic_Main.java
@@ -0,0 +1,16 @@
+package com.claireyurev;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] testCase01 = {"4", "5", "+"};              // 4 + 5 = 9
+        String[] testCase02 = {"10", "4", "5", "+", "-"};   // 10 - 4 + 5 = 1
+        String[] testCase03 = {"10", "4", "5", "+", "*"};   // 10 * (4 + 5) = 90
+        String[] testCase04 = {"90", "4", "5", "+", "/"};   // 90 / (4 + 5) = 10
+
+        Claire claire = new Claire();
+
+        System.out.println(claire.eval(testCase04));
+    }
+}
