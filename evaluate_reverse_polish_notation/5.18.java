fb255efa0cf35a68da8a44c6b1fe8cdbfc6ee03d
Claire Yaroslav Yurev <61437734+ClaireYurev@users.noreply.github.com>
2022-05-18 10:51:44 -0700

    Create 5.18.java

---
+++ evaluate_reverse_polish_notation/5.18.java
@@ -0,0 +1,37 @@
+package com.claireyurev;
+
+class Solution {
+  
+  private static final Map<String, BiFunction<Integer, Integer, Integer>> OPERATIONS = new HashMap<>();
+  
+  static {
+    OPERATIONS.put("+", (a, b) -> a + b);
+    OPERATIONS.put("-", (a, b) -> a - b);
+    OPERATIONS.put("*", (a, b) -> a * b);
+    OPERATIONS.put("/", (a, b) -> a / b);
+  }
+  
+  public int evalRPN(String[] tokens) {
+  
+    Deque<Integer> stack = new ArrayDeque<>();
+    
+    for (String token : tokens) {
+      if (!OPERATIONS.contains(token)) {
+        stack.push(Integer.parseInt(token));
+        continue;
+      }
+      
+      var secondNumber = stack.pop();
+      var firstNumber = stack.pop();
+      
+      BiFunction<Integer, Integer, Integer> operation;
+      operation = OPERATIONS.get(token);
+      
+      var result = operation.apply(firstNumber, secondNumber);
+      stack.push(result);      
+    }
+    
+    return stack.pop();    
+  }
+
+}
