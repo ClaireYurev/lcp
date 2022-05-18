54b25fcdb36f8b2c7e02e7dae941a28c69db66b1
Claire Yaroslav Yurev <61437734+ClaireYurev@users.noreply.github.com>
2022-05-18 16:18:12 -0700

    Create Cake.java

---
+++ fib_recursive_memo/Cake.java
@@ -0,0 +1,25 @@
+package com.claireyurev;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Cake {
+  
+  private Map<Integer, Long> memo = new HashMap<>();
+  
+  public long fib(int n) {
+  
+    if (n <= 2)
+      return 1;
+    
+    if (memo.containsKey(n))
+      return memo.get(n);
+    
+    var result = fib(n - 1) + fib(n - 2);
+     
+    memo.put(n, result);
+    
+    return result;    
+  }
+  
+}
